3d07e344d648bba7bb4f77959756b1982049850a
[zhangxiaoyi422] <[751486002@qq.com]>
2018-04-18 16:12:38 +0800

    test

--- vue-template/src/components/HelloWorld.vue
+++ vue-template/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <button @click="addNum(2)">点赞12312312314444444444421231233</button> </br>
+    <button @click="addNum(2)">点赞123123123144dddddddddddd44444444421231233</button> </br>
     <span>{{agreeNum}}</span>
   </div>
 </template>
